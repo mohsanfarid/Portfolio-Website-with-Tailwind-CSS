13dbb31c861d9b3980ff07c33f0875b4b878d55c
Mohsan <mohsin@techchaps.com>
2023-07-25 18:57:03 +0500

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import Analytics from './components/Analytics';
-import Cards from './components/Cards';
+import Analytics from './components/Analytics'; 
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import Navbar from './components/Navbar'; 
+import Navbar from './components/Navbar';  
 
 function App() {
   return (
